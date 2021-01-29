24949d891d44ad51f9a3b25649d404d2f836ee3f
EC2 Default User <ec2-user@ip-172-31-19-52.ec2.internal>
2021-01-29 06:07:55 +0000

    update

---
+++ spec/test_spec.rb
@@ -0,0 +1,12 @@
+describe '四速演算' do
+  context '足し算' do
+    it '1 + 1 は 2 になる' do
+      expect("原野").to eq "はらの"
+    end
+  end
+  context '足し算 ' do
+    it '1 + 1 は 3 になる' do
+      expect(1 + 2).to eq 3
+    end
+  end
+end
